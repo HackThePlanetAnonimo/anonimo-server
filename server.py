c41583ce71c432a40aaae9b6867a82845c40a22f
Aditya Sridhar <aditya123ss@gmail.com>
2015-08-15 15:04:16 -0700

    professor stuff done

--- server.py
+++ server.py
@@ -58,7 +58,9 @@ def ask_a_question():
     import pdb; pdb.set_trace()
     connection.request('POST', '/1/classes/Questions/', json.dumps({
         "Student_id": str(jsonObj['Student_id']),
-        "Text": str(jsonObj['Text'])
+        "Text": str(jsonObj['Text']),
+        "Status": "unanswered",
+        "Votes": 0
     }), {
            "X-Parse-Application-Id": XParseApplicationId,
            "X-Parse-REST-API-Key": XParseRESTAPIKey,
@@ -146,7 +148,149 @@ def student_sign_out():
     })
     return Response(json.dumps({'success': True}),  mimetype='application/json')  
 
-# Create a model for professor: Name, userid, Password, isLoggedIn
+# ------------- Professors -------------------------------------------
+
+# Ability to add a professor
+# Requires: Email, Name, Password
+# Return: check for boolean "success".
+@app.route('/professor_sign_up', methods=['GET','POST'])
+def professor_sign_up():
+    import pdb; pdb.set_trace()
+    jsonObj = request.json
+    params = urllib.urlencode({"where":json.dumps({
+        "Email": str(jsonObj['Email']),
+    })})
+
+    # Check if Email already taken
+    connection.request('GET', '/1/classes/Professors/?%s' % params, '', {
+        "X-Parse-Application-Id": XParseApplicationId,
+        "X-Parse-REST-API-Key": XParseRESTAPIKey,
+    })
+    result = json.loads(connection.getresponse().read())
+    # Email already exists. Signup fails
+    if len(result['results']) > 0:
+        return Response(json.dumps({'success': False}),  mimetype='application/json')
+
+    connection.request('POST', '/1/classes/Professors/', json.dumps({
+        "Name": str(jsonObj['Name']),
+        "Email": str(jsonObj['Email']),
+        "Password": str(jsonObj['Password']),
+        "isLoggedIn": True
+    }), {
+           "X-Parse-Application-Id": XParseApplicationId,
+           "X-Parse-REST-API-Key": XParseRESTAPIKey,
+    })
+    result = json.loads(connection.getresponse().read())
+    return Response(json.dumps({'success': True, 'Professor_id': result['objectId']}),  mimetype='application/json')
+
+# Ability to sign in
+# Requires: Email, Password
+@app.route('/professor_sign_in', methods=['GET', 'POST'])
+def professor_sign_in():
+    jsonObj = request.json
+    params = urllib.urlencode({"where":json.dumps({
+        "Email": str(jsonObj['Email']),
+        "Password": str(jsonObj['Password'])
+    })})
+    connection.request('GET', '/1/classes/Professors/?%s' % params, '', {
+        "X-Parse-Application-Id": XParseApplicationId,
+        "X-Parse-REST-API-Key": XParseRESTAPIKey,
+    })
+    results = json.loads(connection.getresponse().read())['results']
+
+    # Invalid credentials
+    if len(results) == 0:
+        return Response(json.dumps({'success': False}),  mimetype='application/json')
+
+    professorId = results[0]['objectId']
+    connection.request('PUT', '/1/classes/Professors/'+professorId, json.dumps({
+       "isLoggedIn": True
+    }), {
+       "X-Parse-Application-Id": XParseApplicationId,
+       "X-Parse-REST-API-Key": XParseRESTAPIKey,
+       "Content-Type": "application/json"
+    })
+    return Response(json.dumps({'success': True}),  mimetype='application/json')  
+
+# Expects: Professor_id (Professor's objectId)
+@app.route('/professor_sign_out', methods=['POST'])
+def professor_sign_out():
+    import pdb; pdb.set_trace()
+    jsonObj = request.json
+    professorId = jsonObj['Professor_id']
+    connection.request('PUT', '/1/classes/Professors/'+professorId, json.dumps({
+       "isLoggedIn": False
+    }), {
+       "X-Parse-Application-Id": XParseApplicationId,
+       "X-Parse-REST-API-Key": XParseRESTAPIKey,
+       "Content-Type": "application/json"
+    })
+    return Response(json.dumps({'success': True}),  mimetype='application/json') 
+
+# Create a model for professor: Name, email, Password, isLoggedIn
+@app.route('/get_all_professors', methods=['GET'])
+def get_all_professors():
+    connection.request('GET', '/1/classes/Professors/', '', {
+           "X-Parse-Application-Id": XParseApplicationId,
+           "X-Parse-REST-API-Key": XParseRESTAPIKey,
+         })
+    result = json.loads(connection.getresponse().read())
+    return Response(json.dumps(result),  mimetype='application/json')
+
+# expects: Question_id
+# modifies: Status field of Question model
+@app.route('/answer_a_question', methods=['POST'])
+def answer_a_question():
+    jsonObj = request.json
+    questionId = jsonObj['Question_id']
+    connection.request('PUT', '/1/classes/Questions/'+questionId, json.dumps({
+       "Status": "answered"
+    }), {
+       "X-Parse-Application-Id": XParseApplicationId,
+       "X-Parse-REST-API-Key": XParseRESTAPIKey,
+       "Content-Type": "application/json"
+    })
+    return Response(json.dumps({'success': True}),  mimetype='application/json')  
+
+# expects: Question_id
+# modifies: Votes field of Question model
+@app.route('/vote_a_question', methods=['POST'])
+def vote_a_question():
+    jsonObj = request.json
+    questionId = jsonObj['Question_id']
+    connection.request('PUT', '/1/classes/Questions/'+questionId, json.dumps({
+        "Votes": {
+            "__op": "Increment",
+            "amount": 1
+        }
+    }), {
+       "X-Parse-Application-Id": XParseApplicationId,
+       "X-Parse-REST-API-Key": XParseRESTAPIKey,
+       "Content-Type": "application/json"
+    })
+    return Response(json.dumps({'success': True}),  mimetype='application/json') 
+
+# @app.route('/setup_a_lecture', methods=['POST'])
+# def setup_a_lecture():
+#     jsonObj = request.json
+#     professorId = jsonObj['Professor_id']
+
+    
+
+#     connection.request('PUT', '/1/classes/Questions/'+questionId, json.dumps({
+#         "Votes": {
+#             "__op": "Increment",
+#             "amount": 1
+#         }
+#     }), {
+#        "X-Parse-Application-Id": XParseApplicationId,
+#        "X-Parse-REST-API-Key": XParseRESTAPIKey,
+#        "Content-Type": "application/json"
+#     })
+
+# Professor to an array of students
+
+
 # Create a model for class: Student_id, Professor_id.
 # A class object links a student to a professor
 
