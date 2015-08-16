5badaa4266a4feaef6d29582f8db4eb329d0fb63
Aditya Sridhar <aditya123ss@gmail.com>
2015-08-16 08:38:35 -0700

    id signin

--- server.py
+++ server.py
@@ -270,7 +270,7 @@ def professor_sign_in():
        "X-Parse-REST-API-Key": XParseRESTAPIKey,
        "Content-Type": "application/json"
     })
-    resp = Response(json.dumps({'success': True}),  mimetype='application/json')  
+    resp = Response(json.dumps({'success': True, 'Professor_id': result['objectId']}),  mimetype='application/json')  
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
